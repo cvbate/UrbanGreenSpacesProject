05cb5c43ca478c514874b8398b59529cf4464fbc
cvbate <cbate@clarku.edu>
2024-01-26 19:45:20 -0500

    created readme, analysis.sql without complete comments, created miami map in QGIS

--- analysis.sql
+++ analysis.sql
@@ -23,3 +23,19 @@ FROM planet_osm_polygon
 WHERE leisure = 'park'
 ORDER BY area DESC
 LIMIT 5;
+/* The query lists the top 5 largest green spaces by name and area, which are as follows:
+Crandon Park: 4813915.0 sq m,
+Bill Baggs Cape Florida State Park: 2125852.9 sq m,
+Virginia Key Beach North Point Park: 1683502.5 sq m,
+Matheson Hammock Park: 1037290.2 sq m
+Hialeah Park Race Track: 1000239.7 sq m */
+
+
+
+SELECT SUM(ST_Area(way)) as total_green_space_area
+FROM planet_osm_polygon
+WHERE leisure = 'park';
+/* The total area of green space in Miami is = 17255910.3 */
+
+
+/* https://download.bbbike.org/osm/bbbike/Miami/ */
